4fd363253023ac5c690230b3f87c9d898005f8ec
Adrien Chardon <adrienchardon@mailoo.org>
2016-10-27 17:02:56 +0200

    Slightly better board

--- DebugGui/views.py
+++ DebugGui/views.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import math
 
 from gi.repository import Gtk, Gdk
+import cairo
 
 
 class MyLabel(Gtk.Label):
@@ -15,10 +16,27 @@ class MyLabel(Gtk.Label):
 
 
 class Board(Gtk.DrawingArea):
+    COLORS = {
+        'team1': (1, 1, 0.5),
+        'team2': (0.5, 0.5, 1),
+        'me': (0.3, 0.6, 0.3),
+
+        'grid_sub': (0.7, 0.7, 0.7),
+        'grid_main': (0.5, 0.5, 0.5),
+
+        'board_bg': (0.9, 0.9, 0.9),
+        'board_edge': (0.5, 0.5, 0.5),
+        'elem_bg': (0.8, 0.8, 0.8),
+        'elem_edge': (0.5, 0.5, 0.5),
+    }
+    ROBOT_SIZE = (400, 300)
+
     def __init__(self):
         super(Board, self).__init__()
         self.set_property('margin', 10)
         self.connect('draw', self.on_draw)
+
+        self.eceborg = cairo.ImageSurface.create_from_png('Eceborg.png')
 
     def on_draw(self, canvas, cr):
         try:  # todo: this is ugly, please fix it
@@ -27,7 +45,8 @@ class Board(Gtk.DrawingArea):
             x, y, angle = 0, 0, 0
 
         stuff_to_draw = [
-            self.draw_board,
+            self.draw_board_static,
+            self.draw_board_elem,
             self.draw_robot,
         ]
 
@@ -36,49 +55,191 @@ class Board(Gtk.DrawingArea):
         w = win.get_width()
         h = win.get_height()
 
+        # clear
+        cr.set_source_rgb(0.95, 0.95, 0.95)
+        cr.rectangle(0, 0, w, h)
+        cr.fill()
+
         cr.scale(0.2, 0.2)
+        cr.translate(50, 50)
+
+        cr.set_source_rgb(*self.COLORS['board_edge'])
+        cr.rectangle(-20, -20, 2000+2*20, 3000+2*20)
+        cr.fill()
 
         for callback in stuff_to_draw:
             cr.save()
-            callback(cr, (w, h), (x, y, angle))
+            callback(cr, (x, y, angle))
             cr.restore()
 
-    def draw_board(self, cr, window_size, robot_pos):
-        # clear
-        cr.set_source_rgb(1, 1, 1)
-        cr.rectangle(0, 0, window_size[0], window_size[1])
-        cr.fill()
-
-        # draw board
-        cr.set_source_rgb(0.9, 0.9, 0.9)
-        cr.rectangle(0+50, 0+50, 2000, 3000)
+    def draw_board_static(self, cr, robot_pos):
+        # draw bg
+        cr.set_source_rgb(*self.COLORS['board_bg'])
+        cr.rectangle(0, 0, 2000, 3000)
         cr.fill()
 
         # draw grid
         size = 1
-        cr.set_source_rgb(0.5, 0.5, 0.5)
+        cr.set_source_rgb(*self.COLORS['grid_sub'])
         for x in range(0, 2000+1, 100):
-            cr.rectangle(x+50, 0+50, size, 3000)
+            cr.rectangle(x, 0, size, 3000)
         for y in range(0, 3000+1, 100):
-            cr.rectangle(0+50, y+50, 2000, size)
+            cr.rectangle(0, y, 2000, size)
         cr.fill()
-        cr.set_source_rgb(0, 0, 0)
+        cr.set_source_rgb(*self.COLORS['grid_main'])
         for x in range(0, 2000+1, 1000):
-            cr.rectangle(x+50, 0+50, size, 3000)
+            cr.rectangle(x, 0, size, 3000)
         for y in range(0, 3000+1, 1000):
-            cr.rectangle(0+50, y+50, 2000, size)
+            cr.rectangle(0, y, 2000, size)
+        cr.fill()
+
+        # starting points
+
+        cr.set_source_rgb(*self.COLORS['team1'])
+        cr.rectangle(0, 0, 360, 360)
+        cr.rectangle(0, 360+350, 360, 360)
+        cr.fill()
+
+        cr.set_source_rgb(*self.COLORS['team2'])
+        cr.rectangle(0, 3000-(2*360+350), 360, 360)
+        cr.rectangle(0, 3000-360, 360, 360)
+        cr.fill()
+
+        cr.set_source_rgb(*self.COLORS['elem_edge'])
+        cr.rectangle(360, 0, 22, 360+350)
+        cr.rectangle(360, 3000-(360+350), 22, 360+350)
+        cr.fill()
+
+        # white balls
+
+        cr.set_source_rgb(*self.COLORS['elem_edge'])
+
+        cr.arc(540, 650, 170/2+30, 0, 2*math.pi)
+        cr.arc(540, 2350, 170/2+30, 0, 2*math.pi)
+
+        cr.move_to(2000, 0)
+        cr.arc(2000, 0, 510+30, math.pi/2, -math.pi)
+        cr.line_to(2000, 0)
+        cr.move_to(2000, 3000)
+        cr.arc(2000, 3000, 510+30, math.pi, 3*math.pi/2)
+        cr.line_to(2000, 3000)
+
+        cr.move_to(1870, 1070)
+        cr.arc(1870, 1070, 170/2+30, 0, 2*math.pi)
+        cr.move_to(1870, 1930)
+        cr.arc(1870, 1930, 170/2+30, 0, 2*math.pi)
+
         cr.fill()
 
 
-    def draw_robot(self, cr, window_size, robot_pos):
+        cr.set_source_rgb(*self.COLORS['elem_bg'])
+
+        cr.arc(540, 650, 170/2, 0, 2*math.pi)
+        cr.arc(540, 2350, 170/2, 0, 2*math.pi)
+
+        cr.move_to(2000, 0)
+        cr.arc(2000, 0, 510, math.pi/2, -math.pi)
+        cr.line_to(2000, 0)
+        cr.move_to(2000, 3000)
+        cr.arc(2000, 3000, 510, math.pi, 3*math.pi/2)
+        cr.line_to(2000, 3000)
+
+        cr.move_to(1870, 1070)
+        cr.arc(1870, 1070, 170/2, 0, 2*math.pi)
+        cr.move_to(1870, 1930)
+        cr.arc(1870, 1930, 170/2, 0, 2*math.pi)
+
+        cr.fill()
+
+        # cylinders
+
+        cr.set_source_rgb(*self.COLORS['elem_edge'])
+        cr.rectangle(700-22, 0, 450+2*22, 80+28)
+        cr.rectangle(700-22, 3000-(80+28), 450+2*22, 80+28)
+
+        cr.fill()
+
+        cr.set_source_rgb(*self.COLORS['elem_bg'])
+        cr.rectangle(700, 0, 450, 80)
+        cr.rectangle(700, 3000-80, 450, 80)
+
+        cr.fill()
+
+        cr.set_source_rgb(*self.COLORS['elem_edge'])
+        cr.save()
+        cr.translate(2000, 1500)
+        cr.arc(0, 0, 200+10, math.pi/2, -math.pi/2)
+        cr.rotate(-math.pi/4)
+        cr.rectangle(-600-200, -(80+2*28)/2, 600, (80+2*28))
+        cr.rotate(math.pi/4)
+        cr.rectangle(-600-200, -(80+2*28)/2, 600, (80+2*28))
+        cr.rotate(math.pi/4)
+        cr.rectangle(-600-200, -(80+2*28)/2, 600, (80+2*28))
+        cr.fill()
+        cr.restore()
+
+        cr.set_source_rgb(*self.COLORS['elem_bg'])
+        cr.save()
+        cr.translate(2000, 1500)
+        cr.rotate(-math.pi/4)
+        cr.rectangle(-600-200, -80/2, 600, 80)
+        cr.rotate(math.pi/4)
+        cr.rectangle(-600-200, -80/2, 600, 80)
+        cr.rotate(math.pi/4)
+        cr.rectangle(-600-200, -80/2, 600, 80)
+        cr.fill()
+        cr.restore()
+
+        # rockets
+
+        cr.set_source_rgb(*self.COLORS['elem_edge'])
+
+        cr.arc(1350, 80/2, 80/2, 0, 2*math.pi)
+        cr.arc(1350, 3000-80/2, 80/2, 0, 2*math.pi)
+        cr.move_to(0, 0)
+        cr.arc(80/2, 1150, 80/2, 0, 2*math.pi)
+        cr.move_to(0, 0)
+        cr.arc(80/2, 1850, 80/2, 0, 2*math.pi)
+
+        cr.fill()
+
+    def draw_board_elem(self, cr, robot_pos):
+        cr.set_source_rgb(*self.COLORS['team1'])
+
+        cr.arc(1350, 80/2, 76/2, 0, 2*math.pi)
+        cr.arc(80/2, 1150, 76/2, 0, 2*math.pi)
+        cr.fill()
+        cr.arc(1850-80/2, 800-80/2, 76/2, 0, 2*math.pi)
+
+        cr.fill()
+
+
+        cr.set_source_rgb(*self.COLORS['team2'])
+
+        cr.arc(1350, 3000-80/2, 76/2, 0, 2*math.pi)
+        cr.arc(80/2, 1850, 76/2, 0, 2*math.pi)
+        cr.fill()
+        cr.arc(1850-80/2, 2200-80/2, 76/2, 0, 2*math.pi)
+
+        cr.fill()
+
+    def draw_robot(self, cr, robot_pos):
         x, y, angle = robot_pos
-        robot_size = (400, 300)
 
-        cr.set_source_rgb(0.5, 0.5, 1)
-        cr.translate(x+50+robot_size[0]/2+100, -y+50+robot_size[1]/2+500)
+        cr.translate(x+self.ROBOT_SIZE[0]/2+100, -y+self.ROBOT_SIZE[1]/2+1400)
+
+        cr.save()
+
+        cr.set_source_rgb(*self.COLORS['me'])
         cr.rotate(-angle*math.pi/180 + math.pi/2)
-        cr.rectangle(-robot_size[0]/2, -robot_size[1]/2, robot_size[0], robot_size[1])
+        cr.rectangle(-self.ROBOT_SIZE[0]/2, -self.ROBOT_SIZE[1]/2, self.ROBOT_SIZE[0], self.ROBOT_SIZE[1])
         cr.fill()
+        cr.restore()
+
+        cr.scale(2, 2)
+        cr.set_source_surface(self.eceborg, -74/2, -74/2)
+        cr.paint()
+        cr.restore()
 
 
 class MapView(Gtk.Grid):
